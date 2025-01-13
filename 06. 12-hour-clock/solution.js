ae508774062b8ddc9adb508cc1a03234e246097f
aireensk <aireensk@icloud.com>
2025-01-13 12:52:21 +0000

    Solution6_solved

--- 06. 12-hour-clock/solution.js
+++ 06. 12-hour-clock/solution.js
@@ -2,10 +2,17 @@ function convertTo12HourClock(time) {
   const hours = time.slice(0, 2);
   const minutes = time.slice(-2);
   if (hours > 12) {
-    return `${hours - 12}:${minutes} PM`;
-  } else {
-    return `${hours}:${minutes} AM`;
+    return `${parseInt(hours - 12)}:${minutes} PM`;
+  } else if (hours == 12) {
+    return `${12}:${minutes} PM`;;
+  } else if (hours == "00") {
+    return `${12}:${minutes} AM`;
+  }
+  else {
+    return `${parseInt(hours)}:${minutes} AM`;
   }
 }
 
 module.exports = { convertTo12HourClock };
+
+convertTo12HourClock("12:01");
